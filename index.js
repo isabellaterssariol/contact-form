a92d14588fd74f414a69dc21b93ba62678e070cb
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-05-19 19:04:55 -0300

    Doing more JS validations.
    Validating fields and buttons in JS.

--- index.js
+++ index.js
@@ -3,53 +3,55 @@ const fields = document.querySelectorAll('.f-required')
 const spans = document.querySelectorAll('.s-required')
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 const wordRegex = /\b\w+\b.*\b\w+\b/;
-const errorMark = document.querySelectorAll('#errorPoint');
-const sucessMark = document.querySelectorAll('#sucessPoint');
+const errorMark = document.querySelectorAll('.errorPoint');
+const sucessMark = document.querySelectorAll('.successPoint');
 
-form.addEventListener('sumbit', (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     nameValue();
     emailValue();
     messageValue();
+    buttonCheck();
 });
 
-function error(index){
+function showError(index){
     fields[index].style.border = '2px solid #ff0000';
     spans[index].style.display = 'block';
     errorMark[index].style.display = 'block';
     sucessMark[index].style.display = 'none';
 }
 
-function sucess(index){
+function showSuccess(index){
     fields[index].style.border = '2px solid #008000';
     spans[index].style.display = 'none';
     errorMark[index].style.display = 'none';
-    sucessMark[index].style.display = 'block'
+    sucessMark[index].style.display = 'block';
 }
 
-function nameValue(){
+const nameValue = function() {
     if(!wordRegex.test(fields[0].value))
     {
-        error(0);
+        showError(0);
     } else {
-        sucess(0);
+        showSuccess(0);
     }
 }
 
-function emailValue() {
+const emailValue = function() {
     if(!emailRegex.test(fields[1].value))
     {
-        error(1);
+        showError(1);
     } else {
-        sucess(1);
+        showSuccess(1);
     }
 }
 
-function messageValue() {
+const messageValue = function() {
     if(fields[2].value.length < 20)
     {
-        error(2);
+        showError(2);
     } else {
-        sucess(2);
+        showSuccess(2);
     }
 }
+
